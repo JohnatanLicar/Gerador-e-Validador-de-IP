778080cc9ea644318e04bf4b999abf422c6fd0d2
Johnatan Licar <johnatanlicar@gmail.com>
2023-05-30 18:16:11 -0300

    :recycle: Refatorado

--- GeraIP.py
+++ GeraIP.py
@@ -1,12 +1,40 @@
+import socket
 from random import randint
-ips=''
-for total in range(0, 101):
-    ip = ' '
-    for c in range(0, 4):
-        número = randint(1, 500)
-        if c > 0:
-            ip += '.'
-        ip += str(número)
-    ips += ip + '\n'
-with open('IpsGerados.txt', 'w') as SalvaIps:
-    SalvaIps.write(ips)
+
+def generate_ip():
+    ip = []
+    for _ in range(4):
+        octet = str(randint(0, 255))
+        ip.append(octet)
+    return '.'.join(ip)
+
+def is_valid_ip(ip):
+    parts = ip.split('.')
+    if len(parts) != 4:
+        return False
+    for part in parts:
+        if not part.isdigit() or not 0 <= int(part) <= 255:
+            return False
+    return True
+
+valid_ips = []
+invalid_ips = []
+
+while len(valid_ips) < 100:
+    ip = generate_ip()
+    if is_valid_ip(ip):
+        valid_ips.append(ip)
+    else:
+        invalid_ips.append(ip)
+
+# Salvar todos os IPs gerados em 'ipsGerados.txt'
+with open('ipsGerados.txt', 'w') as file:
+    file.write('\n'.join(valid_ips + invalid_ips))
+
+# Salvar os IPs válidos em 'ipsValidos.txt'
+with open('ipsValidos.txt', 'w') as file:
+    file.write('\n'.join(valid_ips))
+
+# Salvar os IPs inválidos em 'ipsInvalidos.txt'
+with open('ipsInvalidos.txt', 'w') as file:
+    file.write('\n'.join(invalid_ips))
